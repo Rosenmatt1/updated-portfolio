7a0374f32c3bbcab2e0bb74dd0902968c83f8104
Matthew Rosenberg <rosenmatt1@gmail.com>
2021-05-11 18:34:09 -0600

    Got particles working and with css got the particles to not show up in the projects box

--- updated-portfolio/src/components/ParticlesTop.js
+++ updated-portfolio/src/components/ParticlesTop.js
@@ -2,24 +2,19 @@ import React, { Component } from 'react'
 import Particles from 'react-particles-js'
 import '../App.css'
 
-// const particleOpt = {
-//   enable: false,
-//   type: 'outside',
-//   move: {
-//     radius: 1
-//   },
-//   // url: 'path/to/svg.svg'
-// }
-
 const particleOpt = {
-  "type": "outside",
+  enable: true,
+  type: 'outside',
   "particles": {
     "number": {
-      "value": 25
+      "value": 50
     },
     "size": {
-      "value": 3
+      "value": 1
     }
+  },
+  move: {
+    radius: 1
   },
   "interactivity": {
     "events": {
@@ -31,72 +26,18 @@ const particleOpt = {
   }
 }
 
-// const particleOpt = {
-//   "particles": {
-//       "number": {
-//           "value": 60,
-//           "density": {
-//               "enable": true,
-//               "value_area": 1500
-//           }
-//       },
-//       "line_linked": {
-//           "enable": true,
-//           "opacity": 0.1
-//       },
-//       "move": {
-//           "direction": "right",
-//           "speed": 0.05
-//       },
-//       "size": {
-//           "value": 1
-//       },
-//       "opacity": {
-//           "anim": {
-//               "enable": true,
-//               "speed": 1,
-//               "opacity_min": 0.05
-//           }
-//       }
-//   },
-//   "interactivity": {
-//       "events": {
-//           "onclick": {
-//               "enable": true,
-//               "mode": "push"
-//           }
-//       },
-//       "modes": {
-//           "push": {
-//               "particles_nb": 1
-//           }
-//       }
-//   },
-//   "retina_detect": true
-// }
 
 class ParticlesTop extends Component {
-
   render() {
     return (
-      
-      <div className="ui-container">
-        
-        <div className="row particles">
+      <div className="particles">
+        <Particles params={particleOpt} />
 
-        <Particles params={particleOpt} >
-          
-
-          <div className="title">
-            <p className="border">Projects</p>
-          </div>
-
-          </Particles>
-
+        <div className="title projects">
+          <p className="border">Projects</p>
         </div>
-        
-      </div>
-     
+
+      </div >
     )
   }
 }
